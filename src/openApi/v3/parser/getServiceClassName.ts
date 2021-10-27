068c2965c40192825044fde77002965bd1f2a950
Bartłomiej Płaza <bartlomiej.plaza@thulium.pl>
2021-10-27 14:58:41 +0200

    Service generation adjustments

---
+++ src/openApi/v3/parser/getServiceClassName.ts
@@ -0,0 +1,19 @@
+import camelCase from 'camelcase';
+
+/**
+ * Convert the input value to a correct service classname. This converts
+ * the input string to PascalCase and appends the "Service" prefix if needed.
+ */
+export function getServiceClassName(value: string): string {
+    const clean = value
+        .replace(/^[^a-zA-Z]+/g, '')
+        .replace(/[^\w\-]+/g, '-')
+        .replace('_controller', '')
+        .replace('_web_api', '')
+        .trim();
+    const name = camelCase(clean, { pascalCase: true });
+    if (name && !name.endsWith('Service')) {
+        return `${name}Service`;
+    }
+    return name;
+}
